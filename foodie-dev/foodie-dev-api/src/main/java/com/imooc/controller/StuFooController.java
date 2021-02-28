0832125d3b6e8c576dc3ef79e438334039332d04
TinaLand <huihuijiang0619@gmail.com>
2021-02-28 11:39:28 -0500

    updated first restful service

--- foodie-dev/foodie-dev-api/src/main/java/com/imooc/controller/StuFooController.java
+++ foodie-dev/foodie-dev-api/src/main/java/com/imooc/controller/StuFooController.java
@@ -1,19 +1,22 @@
 package com.imooc.controller;
 
 
-//import com.imooc.service.StuService;
+import com.imooc.service.StuService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-//@RestController // json object
-//public class StuFooController {
-//
-//    @Autowired
-//    private StuService stuService;
-//
-//    @GetMapping("/getStu")
-//    public Object hello(int id) {
-//        return stuService.getStuInfo(id);
-//    }
-//}
+@RestController // json object
+public class StuFooController {
+
+    @Autowired
+    private StuService stuService;
+
+    @Transactional(propagation = Propagation.SUPPORTS)
+    @GetMapping("/getStu")
+    public Object getStu(int id) {
+        return stuService.getStuInfo(id);
+    }
+}

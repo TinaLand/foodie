fb857fc17d759c6ebe825f23e2e19aa567113114
TinaLand <huihuijiang0619@gmail.com>
2021-02-28 15:58:23 -0500

    implemented user register

---
+++ foodie-dev/foodie-dev-common/src/main/java/com/imooc/enums/Sex.java
@@ -0,0 +1,20 @@
+package com.imooc.enums;
+
+public enum Sex {
+
+//    woman(0, "female");
+//    man(1, "male");
+//    secret(2, "secret");
+
+    woman(0, "female"),
+    man(1, "male"),
+    secret(2, "secret");
+
+    public final Integer type;
+    public final String value;
+
+    Sex(Integer type, String value) {
+        this.type = type;
+        this.value = value;
+    }
+}
--- foodie-dev/foodie-dev-service/src/main/java/com/imooc/service/impl/UserServiceImpl.java
+++ foodie-dev/foodie-dev-service/src/main/java/com/imooc/service/impl/UserServiceImpl.java
@@ -1,20 +1,26 @@
 package com.imooc.service.impl;
 
+import com.imooc.enums.Sex;
 import com.imooc.mapper.UsersMapper;
 import com.imooc.pojo.Users;
 import com.imooc.pojo.bo.UserBo;
 import com.imooc.service.UserService;
+import com.imooc.utils.DateUtil;
+import com.imooc.utils.MD5Utils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 import tk.mybatis.mapper.entity.Example;
 
+import java.util.Date;
+
 @Service
 public class UserServiceImpl implements UserService {
 
     @Autowired
     public UsersMapper usersMapper;
+    private static final String USER_FACE = "http://122.152.205.72:88/group1/M00/00/05/CpoxxFw_8_qAIlFXAAAcIhVPdSg994.png";
 
     @Transactional(propagation = Propagation.SUPPORTS)
     @Override
@@ -36,8 +42,21 @@ public class UserServiceImpl implements UserService {
     public Users createUser(UserBo userBo) {
         Users user = new Users();
         user.setUsername(userBo.getUsername());
-        user.setPassword(user.getPassword());
+        try {
+            user.setPassword(MD5Utils.getMD5Str(user.getPassword()));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
 
+        // set default nickname same as user name
+        user.setNickname(userBo.getUsername());
+        user.setFace(USER_FACE);
+        user.setBirthday(DateUtil.stringToDate("1900-01-01"));
+
+        user.setSex(Sex.secret.type);
+
+        user.setCreatedTime(new Date());
+        user.setUpdatedTime(new Date());
 
         return null;
     }
